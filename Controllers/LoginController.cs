1247e3a5c6cbd9ba81037c258edfff39d2bfe3df
Swathipriya-kanmani <62537290+Swathipriya-kanmani@users.noreply.github.com>
2022-10-29 12:24:51 +0530

    Add files via upload

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,64 @@
+﻿using AirlinesProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace AirlinesProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly WebsiteAirlineContext db;
+        private readonly ISession session;
+
+        public LoginController(WebsiteAirlineContext _db)  //constructor
+        {
+            db = _db;
+            
+
+        }
+
+
+
+        [HttpGet]
+        public IActionResult login()
+        {
+            //UserLogin emp = UserLogin.GetSingleEmployeeInfo();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UserLogin l)
+        {
+            var result = (from i in db.UserLogins
+                          where i.UserName == l.UserName && i.Password == l.Password
+                          select i).SingleOrDefault();
+            if (result != null)
+            {
+                //HttpContext.Session.SetString("username", l.UserName);
+                return RedirectToAction("Details", "User");
+            }
+
+            return null;
+
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(UserLogin l)
+        {
+            db.UserLogins.Add(l);
+            db.SaveChanges();
+            return RedirectToAction("login");
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("login");
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AirlinesProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Security.AccessControl;
+
+
+
+namespace AirlinesProject.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly WebsiteAirlineContext _db;
+        
+        public UserController(WebsiteAirlineContext db)  //constructor
+        {
+            _db = db;
+        }
+
+        
+        public IActionResult Index()
+        {
+            
+            if (ViewBag.MyName != null)
+            {
+                return View(_db.FlightBookings.ToList());
+            }
+            else
+            {
+                return RedirectToAction("details", "User");
+            }
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(FlightBooking fb)
+        {
+            _db.FlightBookings.Add(fb);//to add a record to the context
+            _db.SaveChanges();//to add a record to the database permanently
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int planeid)
+        {
+            //AirplaneInfo u = _db.AirplaneInfos.Find(planeid);
+            return View(_db.AirplaneInfos.ToList());
+        }
+
+        
+       
+
+         
+        public IActionResult Delete(int id)
+        
+        {
+            //TicketReservation t = _db.TicketReservations.Find(id);
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            TicketReservation t = _db.TicketReservations.Find(id);
+            _db.TicketReservations.Remove(t);
+            _db.SaveChanges();
+            return RedirectToAction("Details");
+
+        }
+
+        public IActionResult Payment()
+        {
+            return View();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
+
+
